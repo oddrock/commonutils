8fe9e551b461f93ad1eaf910f974bc9563703208
oddrock <oddrock@gmail.com>
2017-09-24 16:26:14 +0800

    test

--- src/main/java/com/oddrock/common/file/FileUtils.java
+++ src/main/java/com/oddrock/common/file/FileUtils.java
@@ -7,7 +7,6 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.util.Date;
 
 public class FileUtils {
 	/**
