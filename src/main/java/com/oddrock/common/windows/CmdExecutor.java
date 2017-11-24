8ea19cef93b9e7168fba891cc50e626a312a4f57
oddrock <oddrock@gmail.com>
2017-11-24 17:17:02 +0800

    test

--- src/main/java/com/oddrock/common/windows/CmdExecutor.java
+++ src/main/java/com/oddrock/common/windows/CmdExecutor.java
@@ -1,6 +1,9 @@
 package com.oddrock.common.windows;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class CmdExecutor {
 	private static final CmdExecutor instance = new CmdExecutor();
@@ -32,5 +35,24 @@ public class CmdExecutor {
 	 */
 	public CmdResult openDirWindows(String path) {
 		return exeCmd("explorer \"" + path + "\"");
+	}
+	
+	/**
+	 * 检测指定APP是否已打开
+	 * @param appname	
+	 * @throws IOException 
+	 */
+	public boolean isAppAlive(String appname) throws IOException {
+		Process p = Runtime.getRuntime().exec("tasklist /svc");  
+		InputStream is = p.getInputStream(); 
+		InputStreamReader ir = new InputStreamReader(is, "GBK");  
+		BufferedReader br = new BufferedReader(ir);
+		String line = null;
+		while((line=br.readLine())!=null) {
+			if(line.split("\\s+")[0].equalsIgnoreCase(appname)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
