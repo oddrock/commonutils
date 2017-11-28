62df0d3270c68b5bd18ffbfd85cb91c25e93329e
oddrock <oddrock@gmail.com>
2017-11-29 00:16:24 +0800

    test

--- src/main/java/com/oddrock/common/windows/CmdExecutor.java
+++ src/main/java/com/oddrock/common/windows/CmdExecutor.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CmdExecutor {
 	private static final CmdExecutor instance = new CmdExecutor();
@@ -74,5 +76,35 @@ public class CmdExecutor {
 	 */
 	public void exportTasklistToFile(File dstFile) throws IOException {
 		CmdExecutor.getSingleInstance().exeCmd("tasklist /svc > \""+dstFile.getCanonicalPath() + "\"");
+	}
+	
+	/**
+	 * 获得进程号
+	 * @param appname
+	 * @return
+	 * @throws IOException
+	 */
+	public String getProcNum(String appname) throws IOException {
+		Process p = Runtime.getRuntime().exec("tasklist /svc");  
+		InputStream is = p.getInputStream(); 
+		InputStreamReader ir = new InputStreamReader(is, "GBK");  
+		BufferedReader br = new BufferedReader(ir);
+		String line = null;
+		Pattern pattern = Pattern.compile("\\d+");   
+		while((line=br.readLine())!=null) {
+			// 按数字划分才不会让名字中间有空格的appname被错误分割
+			if(line.split("\\d+")[0].trim().equalsIgnoreCase(appname.trim())) {
+				Matcher mather = pattern.matcher(line); 
+				if(mather.find()) {
+					return mather.group(0);
+				}
+
+			}
+		}
+		return null;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		System.out.println(CmdExecutor.getSingleInstance().getProcNum("FineReader.exe"));
 	}
 }
