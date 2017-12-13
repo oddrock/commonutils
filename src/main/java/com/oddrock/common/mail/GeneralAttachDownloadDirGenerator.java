6ab5453d83148d81fd59fed8c0a366053431b15a
oddrock <oddrock@gmail.com>
2017-12-14 07:12:37 +0800

    tr

--- src/main/java/com/oddrock/common/mail/GeneralAttachDownloadDirGenerator.java
+++ src/main/java/com/oddrock/common/mail/GeneralAttachDownloadDirGenerator.java
@@ -11,7 +11,7 @@ public class GeneralAttachDownloadDirGenerator implements AttachDownloadDirGener
 	// 目录的格式是<时间字符串>-==-发件人邮箱地址-==-邮件主题
 	public File generateDir(File baseDir, MailRecv mail) {
 		String separator = "]--[";
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd"); 
+		SimpleDateFormat format = new SimpleDateFormat("MM月dd日HH时mm分"); 
 		String dateStr = format.format(new Date());
 		String lastDirName = dateStr + separator + SensitiveStringUtils.replaceSensitiveString(mail.getFromNick()) + separator + mail.getFrom() + separator + SensitiveStringUtils.replaceSensitiveString(mail.getSubject());
 		lastDirName = lastDirName.trim();
