78a697d9be56fac0a3380517a541d7d0eed7963c
oddrock@gmail.com <qzfeng@ustcinfo.com>
2019-08-23 22:43:34 +0800

    File

--- src/main/java/com/oddrock/common/file/FileUtils.java
+++ src/main/java/com/oddrock/common/file/FileUtils.java
@@ -735,7 +735,7 @@ public class FileUtils {
 		 * getAllFilesAbsoultePathRecursively("C:\\Users\\oddro\\Desktop\\熊逸书院" )){
 		 * System.out.println(path); }
 		 */
-		gatherAllFiles("X:\\学习视频\\马徐骏新知报告（第二季）");
+		gatherAllFiles("X:\\百度云\\熊逸书院（第一季）");
 		/*
 		 * gatherAllFiles("C:\\Users\\oddro\\Desktop\\关系攻略",
 		 * "C:\\Users\\oddro\\Desktop\\得到\\关系攻略", true);
